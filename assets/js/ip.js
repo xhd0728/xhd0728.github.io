45a12422642feff2063a70f14bdfa506edf8416c
Haidong Xin <hdxin2002@gmail.com>
2024-11-07 17:58:06 +0800

    update

--- assets/js/ip.js
+++ assets/js/ip.js
@@ -1,12 +1,9 @@
 async function fetchVisitorIP() {
-    console.log('lbq');
     try {
-        console.log('000');
-        const response = await fetch('http://ip-api.com/json/?lang=zh-CN');
-        console.log('123')
+        const ipData = await fetch('https://api.ipify.org?format=json');
+        const ip = await ipData.json();
+        const response = await fetch(`https://ip.xinhaidong.top?ip=${ip.ip}`);
         const data = await response.json();
-        console.log('456')
-        console.log(data)
         if (data) {
             document.getElementById('visitor-ip').textContent = `${data.query}`;
             document.getElementById('visitor-address').textContent = `${data.country} ${data.regionName} ${data.city}`;
